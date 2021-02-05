7d129ed7f28ce746a619192f05099703c7a732fb
Fun Planet <dipamdiptam@gmail.com>
2021-02-05 14:20:59 +0530

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,16 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
+    "plugin:vue/vue3-essential",
+    "@vue/airbnb",
+    "@vue/typescript/recommended",
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    quotes: ['error', 'double']
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    quotes: ["error", "double"],
   },
 };
