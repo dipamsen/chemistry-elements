ace4e46c73f39fe7ef6b456a5d29b78bf2c96b69
Fun Planet <dipamdiptam@gmail.com>
2021-02-05 20:19:31 +0530

    added github, electron config

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="container">
-    <h1>Periodic Table</h1>
-    <p>Hi Amey</p>
-    <div class="row">
-      <div class="input-field col s12">
-        <input v-model="query" id="query" type="text" />
-        <label class="active" for="query">Enter Name/Symbol/Number</label>
+    <h1>Element Explorer</h1>
+    <form @submit.prevent="submit">
+      <div class="row">
+        <div class="input-field col s12">
+          <input v-model="query" id="query" type="text" />
+          <label class="active" for="query">Enter Name/Symbol/Number</label>
+        </div>
       </div>
-    </div>
-    <button class="btn" @click="submit">Search</button>
+      <button class="btn" type="submit">Search</button>
+    </form>
     <h1></h1>
     <div class="table" v-if="data.name">
+      <img
+        class="bohr"
+        :src="`https://upload.wikimedia.org/wikipedia/commons/${md5Hash}/Electron_shell_${data.number
+          .toString()
+          .padStart(3, '0')}_${data.name}.svg`"
+        alt="Bohr model of {{data.name}}"
+      />
+      <br />
       {{ data.summary }}<br />
       <a :href="data.source" class="btn-small red">Wikipedia</a>
       <table>
@@ -27,8 +36,19 @@
           <td>{{ data.atomic_mass.toFixed(0) }}</td>
         </tr>
         <tr>
-          <th>Electronic Configuration</th>
+          <th>Shell Structure</th>
           <td>{{ data.shells.join(", ") }}</td>
+        </tr>
+        <tr>
+          <th>Electronic Configuration</th>
+          <td
+            v-html="
+              data.electron_configuration.split` `.map((x) => {
+                [, f, l] = x.match(/(\d+(?:s|p|d|f))(\d+)/);
+                return `${f}<sup>${l}</sup>`;
+              }).join` `
+            "
+          ></td>
         </tr>
         <tr>
           <th>State</th>
@@ -40,15 +60,25 @@
         </tr>
       </table>
     </div>
-    <div class="error white-text" v-if="error">{{ error }}</div>
+    <a href="https://github.com/dipamsen/chemistry-elements">
+      <img
+        loading="lazy"
+        width="120"
+        height="120"
+        src="https://github.blog/wp-content/uploads/2008/12/forkme_right_white_ffffff.png"
+        class="github hide-on-med-and-down show-on-large"
+        alt="Fork me on GitHub"
+        data-recalc-dims="1"
+      />
+    </a>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import { defineComponent, ref } from "vue";
 import table from "@/assets/periodicTable.json";
+import md5 from "md5";
 
-type Elt = typeof table.elements[number];
 export default defineComponent({
   name: "App",
   setup() {
@@ -70,7 +100,7 @@ export default defineComponent({
     };
   },
   methods: {
-    getElement(nameOrSymbol: string): Elt {
+    getElement(nameOrSymbol) {
       if (!Number.isNaN(parseInt(nameOrSymbol, 10))) {
         const z = parseInt(nameOrSymbol, 10);
         if (z < 0 || z > this.elements.length) {
@@ -95,6 +125,15 @@ export default defineComponent({
       }
     },
   },
+  computed: {
+    md5Hash() {
+      const fileName = `Electron_shell_${this.data.number
+        .toString()
+        .padStart(3, "0")}_${this.data.name}.svg`;
+      const hash = md5(fileName);
+      return `${hash[0]}/${hash[0]}${hash[1]}`;
+    },
+  },
 });
 </script>
 
@@ -107,6 +146,18 @@ body {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.bohr {
+  min-width: 200px;
+  width: 60%;
+  max-width: 350px;
+}
+
+.github {
+  top: 0;
+  right: 0;
+  position: absolute;
 }
 
 #app {
